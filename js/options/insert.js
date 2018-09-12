0e079e1276d65def07682f2c4d2494032dec6804
耿少靖 <1442150545@qq.com>
2018-09-12 11:15:35 +0800

    添加在线接口功能

--- js/options/insert.js
+++ js/options/insert.js
@@ -1,9 +1,18 @@
 $.ajax({
-    type: "method",
-    url: "url",
-    data: "data",
-    dataType: "dataType",
+    type: "GET",
+    url: "https://yhtx1997.gitee.io/video_parsing_api/",
     success: function(response) {
-
+        console.log(response);
+        var response = JSON.parse(response);
+        console.log(response);
+        if (response.ret && response.title === "接口数据") {
+            var response = response.data;
+            var jiekou = $("#jiekou");
+            for (x in response) {
+                var data = response[x]
+                var str = "<option value='" + data.href + "'>" + data.title + "</option>"
+                $(jiekou).append(str);
+            }
+        }
     }
 });
--- js/options/options.js
+++ js/options/options.js
@@ -20,18 +20,19 @@ function save_options() {
 // stored in chrome.storage.
 function restore_options() {
     // Use default value jiekou = 'red' and likesjiekou = true.
+
     chrome.storage.sync.get({
         defautlt_jiekou: 'http://jx.598110.com/index.php?url='
     }, function(items) {
         $('#jiekou').val(items.defautlt_jiekou);
         var status = document.getElementById('status');
-        status.textContent = '已读取最新配置';
-
+        status.textContent = '已获取当前接口';
         setTimeout(function() {
             status.textContent = '';
         }, 750);
     });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click',
     save_options);
